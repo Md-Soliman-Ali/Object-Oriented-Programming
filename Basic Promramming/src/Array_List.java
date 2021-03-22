e17dcb5104e749a6735822b94bd1a56e3e47260b
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-03-22 21:28:18 +0600

    Add files via upload

---
+++ Basic Promramming/src/Array_List.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Array_List {
+	
+public static void main(String[] args) {
+		
+		ArrayList <String> list = new ArrayList <String>();
+		
+		list.add("AWE");
+		list.add("WEB");
+		list.add("Security");
+		
+		for (String arr : list)
+		{
+			System.out.println(arr);
+		}
+		
+	}
+
+}
