9b0f1435ae8f4ecc73f2c7a56c98dfeda32bc152
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-10-23 10:15:56 +0600

    Add files via upload

---
+++ Java Basic/src/main/java/basic/Hello_Java.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Hello_Java {
+    public static void main(String[] args) {
+        // System.out.println("Hello Java");
+
+        String myVar = "Hello Java";
+        System.out.println(myVar);
+    }
+}
